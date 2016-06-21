7c5f410b2693b3436773aac938f2d4f0c68c8a85
imkaruna <imkaruna@gmail.com>
2016-06-21 10:25:30 -0400

    Updated LogsController and view

--- app/assets/javascripts/controllers/LogsController.js
+++ app/assets/javascripts/controllers/LogsController.js
@@ -3,19 +3,19 @@ function LogsController($scope) {
   $scope.logs = {
       1: {
         name: 'Sam',
-        books_read: ['Book1', 'Book2']
+        books_read: [{name: 'Book1', minutes: '10'}, {name: 'Book2', minutes: '25'}]
       },
       2: {
         name: 'Dave',
-        books_read: ['Book2', 'Book4']
+        books_read: [{name: 'Book2', minutes: '23'}, {name: 'Book4', minutes: '12'}]
       },
       3: {
         name: 'Smith',
-        books_read: ['Book3', 'Book4', 'Book5']
+        books_read: [{name: 'Book3', minutes: '22'}, {name: 'Book4', minutes: '11'}, {name: 'Book5', minutes: '12'}]
       },
       4: {
         name: 'Anu',
-        books_read:  ['Book2', 'Book4']
+        books_read:  [{name: 'Book2', minutes: '22'}, {name: 'Book4', minutes: '33'}]
       }
     };
 }
