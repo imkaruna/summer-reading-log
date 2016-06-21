043b6085be97efebbeb835b91182b7fe3aa807b9
imkaruna <imkaruna@gmail.com>
2016-06-21 16:25:26 -0400

    Update js and nested views for reader model

--- app/assets/javascripts/controllers/ReadersController.js
+++ app/assets/javascripts/controllers/ReadersController.js
@@ -1,11 +1,33 @@
-function ReadersController($scope) {
+function ReadersController($scope, $resource, $http, $stateParams) {
   console.log('reader');
-  $scope.readers = [{name: 'Sam'}, {name: 'Dave'}, {name: 'Smith'}, {name: 'Anu'}];
+  Reader = $resource("/readers/:id", {id: "@id"}, {update: {method: "PUT"}});
+  $scope.readers = Reader.query();
   $scope.addReader = function () {
     console.log($scope.newReader);
-    $scope.readers.push($scope.newReader);
+    reader = Reader.save($scope.newReader);
+    $scope.readers.push(reader);
     $scope.newReader = '';
-  }
+  };
+
+  $scope.updateReader = function (reader) {
+    $scope.refresh();
+    Reader.update(reader);
+  };
+
+  $scope.deleteReader = function (reader) {
+    Reader.delete({ id: reader.id }, function() {
+    console.log(reader.id + ' Deleted from server');
+    $scope.refresh();
+  });
+  };
+
+  $scope.refresh = function(){
+    $http.get('/readers')
+          .success(function(data){
+               $scope.readers = data;
+          });
+  };
+
 }
 
 angular
--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -1,2 +1,33 @@
 class ReadersController < ApplicationController
+  skip_before_action :verify_authenticity_token, if: :json_request?
+  respond_to :json
+
+  def index
+    respond_with Reader.all
+  end
+
+  def create
+    respond_with Reader.create(reader_params)
+  end
+
+  def update
+    @reader = Reader.find(params[:id])
+    respond_with @reader.update(reader_params)
+  end
+
+  def destroy
+    @reader = Reader.find(params[:id])
+    @reader.destroy
+    respond_with Reader.all
+  end
+
+  private
+  def reader_params
+    params.require(:reader).permit(:name)
+  end
+
+  protected
+  def json_request?
+    request.format.json?
+  end
 end
