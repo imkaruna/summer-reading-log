320f9998434c4055b2da281d212d2e056f27987b
imkaruna <imkaruna@gmail.com>
2016-06-21 21:17:34 -0400

    Added methods

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,4 +1,12 @@
 class Reader < ActiveRecord::Base
   has_many :reader_books
   has_many :books, through: :reader_books
+
+  def books_read
+    self.books
+  end
+
+  def reading_log
+    self.reader_books.collect{|b| [Book.find_by_id(b.book_id).name, b.minutes]}.flatten
+  end
 end
