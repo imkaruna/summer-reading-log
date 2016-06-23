5e03f6c9ad7899ed7ec5b9ea0be7697f55cee0ff
Karuna <imkaruna@gmail.com>
2016-06-23 17:10:51 -0400

    Added select options for Book Genre

--- app/assets/javascripts/controllers/BooksController.js
+++ app/assets/javascripts/controllers/BooksController.js
@@ -2,8 +2,23 @@ function BooksController($scope, $resource, $http) {
   var ctrl = this;
   console.log('books');
   Book = $resource("/books/:id", {id: "@id"}, {update: {method: "PUT"}});
+  $scope.data = {
+    repeatSelect: null,
+    genreOptions: [
+      {name: 'Science fiction'},
+      {name: 'Action and Adventure'},
+      {name: 'Satire'},
+      {name: 'Romance'},
+      {name: 'Mystery'},
+      {name: 'Horror'},
+      {name: 'Self help'},
+      {name: 'Drama'}
+    ]
+   };
+
   $scope.books = Book.query();
   $scope.title = "All Books";
+
   $scope.addBook = function () {
     reader = Book.save($scope.newBook);
     console.log($scope.newBook);
@@ -11,10 +26,12 @@ function BooksController($scope, $resource, $http) {
     $scope.refresh();
     $scope.newBook = '';
   }
+
   $scope.refresh = function(){
     $http.get('/books')
           .success(function(data){
-               ctrl.books = data;
+            console.log(data);
+            ctrl.books = data;
           });
   };
 
