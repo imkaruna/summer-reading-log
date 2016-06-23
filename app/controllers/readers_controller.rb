101cef6b17a89f41b0834e6ece5af0a5b7f452fa
Karuna <imkaruna@gmail.com>
2016-06-23 00:03:28 -0400

    Fixed controller for Reader update

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -19,13 +19,15 @@ class ReadersController < ApplicationController
 
   def update
     @reader = Reader.find(params[:id])
-    params[:books].each do |book|
-      book_to_add = Book.find_or_create_by(id: book["id"])
-      unless @reader.books.include?(book_to_add)
-        @reader.books << book_to_add
+    if params[:books]
+      params[:books].each do |book|
+        book_to_add = Book.find_or_create_by(id: book["id"])
+        unless @reader.books.include?(book_to_add)
+          @reader.books << book_to_add
+        end
       end
     end
-    @reader.update!(reader_params)
+    @reader.update(reader_params)
     respond_to do |format|
       format.json {render :json => @reader.to_json(:only => [:id, :name], :include => [:books])}
     end
