25de01e4d37153fd43e17d9b77cdaf0485af5d9e
Karuna <imkaruna@gmail.com>
2016-06-25 22:24:11 -0400

    Added check for book assignment

--- app/assets/javascripts/controllers/ReadersProfileController.js
+++ app/assets/javascripts/controllers/ReadersProfileController.js
@@ -1,5 +1,6 @@
 function ReadersProfileController($scope, profile, $filter, $stateParams, $resource, $http) {
   var reader = this;
+  reader.unread = true;
   Reader = $resource("/readers/:id", {id: "@id"}, {update: {method: "PUT"}});
   Book = $resource("/books/:id", {id: "@id"}, {update: {method: "PUT"}});
   reader.data = profile.data;
@@ -11,19 +12,34 @@ function ReadersProfileController($scope, profile, $filter, $stateParams, $resou
 
   reader.allbooks = Book.query();
 
+  reader.hasBook = function (bookId) {
+    Reader.get({id:this.data.id}).$promise.then(function (user) {
+      if (!user.books.map(function (book) {
+        return book.id}).includes(bookId)){
+          return true;
+      }
+      else {
+        return false;
+      }
+
+    });
+  }
+
   reader.assignBook = function (book) {
     newBook = book;
-    userBooks = profile.data.books;
     Reader.get({id:this.data.id}).$promise.then(function (user) {
-      if (!user.books.includes(newBook)){
+      if (!user.books.map(function (book) {
+        return book.id}).includes(newBook.id)){
         user.books.push(newBook);
         Reader.update(user);
+        reader.message = "Book added to user's list!";
+        reader.refresh();
       }
       else {
         console.log("Reader has read this book!");
+        alert("Reader has read this book");
       }
 
-      reader.refresh();
     });
   }
 
